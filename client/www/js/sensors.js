cc84ec3b4d2f432a92f937b327a83221cd9f6b9d
Mihai Oltean <mihai.oltean@gmail.com>
2017-09-24 14:05:15 +0300

    test is sensors are available

--- client/www/js/sensors.js
+++ client/www/js/sensors.js
@@ -9,34 +9,35 @@ function on_device_orientation(event)
 {
 	var c_sensor_position = document.getElementById("c_sensor_position");
 	
-	var alpha = Math.round(event.alpha);
-	var beta = Math.round(event.beta);
-	var gamma = Math.round(event.gamma);
-  
-	if (alpha != null && beta != null && gamma != null){
-		
-		c_sensor_position.setAttribute("cx", sensor_bbox_center.x + gamma * sensor_bbox_width / 180);
-		c_sensor_position.setAttribute("cy", sensor_bbox_center.y + beta * sensor_bbox_height / 180);
-		
-		if (beta > -90 && beta < 90)
-			if (Math.abs(previous_gamma - gamma) > 5 || Math.abs(previous_beta - beta) > 5){
-				var bx = (gamma / 2.9 + 32);
-				var by = (beta / 2.9 + 32);
+	if (event.beta != null){// make no sense to continue if I don't have access to sensors
+		var alpha = Math.round(event.alpha);
+		var beta = Math.round(event.beta);
+		var gamma = Math.round(event.gamma);
+	  
+		if (alpha != null && beta != null && gamma != null){
+			
+			c_sensor_position.setAttribute("cx", sensor_bbox_center.x + gamma * sensor_bbox_width / 180);
+			c_sensor_position.setAttribute("cy", sensor_bbox_center.y + beta * sensor_bbox_height / 180);
+			
+			if (beta > -90 && beta < 90)
+				if (Math.abs(previous_gamma - gamma) > 5 || Math.abs(previous_beta - beta) > 5){
+					var bx = (gamma / 2.9 + 32);
+					var by = (beta / 2.9 + 32);
 
-				var b = new Uint8Array(2);
-				b[0] = bx;
-				b[1] = by;
-			
-				b[0] |= (1 << 7);
-				b[1] |= (1 << 6);
-			
-				send_command_to_robot(b[0], b[1]);
-				console.log(gamma + " " + beta + " " + b[0] + " " + b[1]);
-			
-				previous_gamma = gamma;
-				previous_beta = beta;
-			}
-
+					var b = new Uint8Array(2);
+					b[0] = bx;
+					b[1] = by;
+				
+					b[0] |= (1 << 7);
+					b[1] |= (1 << 6);
+				
+					send_command_to_robot(b[0], b[1]);
+					console.log(gamma + " " + beta + " " + b[0] + " " + b[1]);
+				
+					previous_gamma = gamma;
+					previous_beta = beta;
+				}
+		}
 	}
 }
 //-------------------------------------------------
