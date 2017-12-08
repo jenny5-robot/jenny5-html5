03d63efa239ae71265a50812e008a697a3f258d8
Mihai Oltean <mihai.oltean@gmail.com>
2017-12-08 22:25:31 +0200

    send pause when capturing a picture

--- client/www/js/camera.js
+++ client/www/js/camera.js
@@ -3,6 +3,12 @@
 function send_capture_head_camera()
 {
 	send_command_to_robot(1 << 7, CAPTURE_HEAD_CAMERA);
+	send_pause_robot();
 	document.getElementById("img_camera").src = "head.jpg?t=" + Math.random();
 }
 //--------------------------------------------
+function send_capture_left_arm_camera()
+{
+	send_command_to_robot(1 << 7, CAPTURE_LEFT_ARM_CAMERA);	
+}
+//--------------------------------------------------------------------
